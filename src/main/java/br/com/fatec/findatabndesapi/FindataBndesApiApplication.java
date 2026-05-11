e1f7140ac1446d8b0060d3da001c7e9b62e56f76
Luiz Cruz <luizantoniojcruz@gmail.com>
2026-05-11 14:45:26 -0300

    Commit Inicial do Projeto

---
+++ src/main/java/br/com/fatec/findatabndesapi/FindataBndesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.findatabndesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindataBndesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindataBndesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fatec/findatabndesapi/FindataBndesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.findatabndesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindataBndesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
